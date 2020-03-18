4b13a2f0d0310d51ce09a5b4dc331a76a3518ee4
shihuairenai <https://git.hypers.com/frontend-web/unilever-fe.git>
2020-03-18 16:40:29 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">
+    <a-button type="primary">Button</a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang='less'>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+import Vue from 'vue'
+import { Button } from 'ant-design-vue';
+import App from './App'
+
+Vue.component(Button.name, Button)
+ 
+Vue.config.productionTip = false
+ 
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
